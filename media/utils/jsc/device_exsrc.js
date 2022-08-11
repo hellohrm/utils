6ce0fe82e679c378d076883f5e56d62fdadad9e5
caicangcua <36462470+caicangcua@users.noreply.github.com>
2022-08-11 20:26:41 +0700

    v2

---
+++ media/utils/jsc/device_exsrc.js
@@ -0,0 +1,52 @@
+﻿var w0w = window,
+    _gsC = function (url, filetype, success, id) {
+        var script, head = document.getElementsByTagName('head')[0], done = false;
+        if (id && id != '' && w0w.dynload && w0w.dynload.indexOf(id) > -1) {
+            done = true;
+            success();
+        } else {
+            if (filetype == "js") {
+                script = document.createElement('script');
+                script.setAttribute("type", "text/javascript");
+                script.setAttribute("src", url);
+            } else if (filetype == "css") {
+                script = document.createElement("link");
+                script.setAttribute("rel", "stylesheet");
+                script.setAttribute("type", "text/css");
+                script.setAttribute("href", url);
+            } else {
+                return;
+            };
+            if (id) { script.setAttribute("id", id); if (!w0w.dynload) w0w.dynload = []; w0w.dynload.push(id); };
+            script.onload = script.onreadystatechange = function () {
+                if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+                    done = true;
+                    success();
+                    script.onload = script.onreadystatechange = null;
+                    if (filetype == 'js') {
+                        try {
+                            head.removeChild(script);
+                        } catch (err) {
+                        };
+                    };
+                }
+            };
+            script.onerror = function (e) {
+                // w0w.location.replace( '//' + ((w0w.location.hostname.indexOf('www.') > -1) ? 'www.' : '') + 'hrpro.cf/unluckyday');
+            }
+            head.appendChild(script);
+        }
+    };
+
+//try {
+//    _gsC('https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.js', 'js', function () {
+//        _gsC('https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css', 'css', function () {
+//            window['loadSWeet']();
+//        }, 'sweetalert2.min.css');
+//    }, 'sweetalert2.min.js');
+//} catch (err) {
+//    console.log('close err');
+//}
+
+
+alert('here');
